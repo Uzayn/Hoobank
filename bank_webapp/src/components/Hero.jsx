089772815f39662d6361f8f368808d8d46499f73
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-09-18 21:01:35 +0100

    Hero section

--- bank_webapp/src/components/Hero.jsx
+++ bank_webapp/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import {discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -12,14 +13,30 @@ const Hero = () => (
         <span className="text-white">1 Month</span> Account
         </p>
       </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+        The Next <br className="sm:block hidden" /> {" "}
+        <span className="text-gradient" >Generation</span> {" "} <br className="sm:block hidden" />
+        </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
+      </div>
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
+        We examine annual percentage rates, annual fees.
+      </p>
     </div>
 
-    <div className="flex flex-row justify-between items-center w-full">
-      <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white">
-      The Next <br className="sm:block hidden" /> {" "}
-      <span className="text-gradient" >Generation</span> {" "} <br className="sm:block hidden" />
-      Payment Method.
-      </h1> 
+    <div>
+      <img src={robot} alt="robot" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
+      <div className="absolute z-[1] w-[40%] h-[35%] top-0 white__gradient"/>
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 blue__gradient"/>
     </div>
   </section>
   )
