c369f74c6eeecf0c0c5eb2de5f0aa23c8bc94a36
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-09-19 12:52:14 +0100

    complete hero and stats section

--- bank_webapp/src/components/Business.jsx
+++ bank_webapp/src/components/Business.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, {layout} from '../style'
+import Button from './Button'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>
+          You do the business, <br className='sm:block hidden' /> we'll handle the money.
+        </h2>
+      </div>
+    </section>
   )
 }
 
